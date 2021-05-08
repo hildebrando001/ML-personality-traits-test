95f558aa50421f4782c751bee05c7f29b10003a1
José Hildebrando <jose.hildebrando@gmail.com>
2021-05-08 10:20:38 -0300

    create function run_algorithm

--- Personality_Test.py
+++ Personality_Test.py
@@ -19,7 +19,7 @@ st.sidebar.header('Answer the questions')
 def user_input_features(qlist):
     questionsDict = {}
     for q in qlist:
-        questionsDict[q] = st.sidebar.slider(q, 1, 5, 3)
+        questionsDict[q] = st.sidebar.slider(q, 1, 5, 5)
     questionsDF = pd.DataFrame(questionsDict, index=[0])
     return questionsDF
 
@@ -29,10 +29,13 @@ data = get_data("data-final.csv")
 
 df_questions = user_input_features(engList)
 # st.subheader('User inputs')
-st.write(data[:5])
 
-kmeans = KMeans(n_clusters=5)
-k_fit = kmeans.fit(data)
+def run_algorithm():
+    kmeans = KMeans(n_clusters=5)
+    k_fit = kmeans.fit(data)
+    predict = k_fit.predict(df_questions)
 
-st.write(kmeans.labels_)
+    st.write(predict)
 
+if st.sidebar.button('Analyse'):
+    run_algorithm()
