f51c65015dfaaa077bbf49aff818a709ae70dc30
hildebrando001 <jose.hildebrando@gmail.com>
2021-06-12 20:29:04 -0300

    chart auto size

--- Personality_Test.py
+++ Personality_Test.py
@@ -4,7 +4,7 @@ from sklearn.cluster import KMeans
 import plotly.express as px
 import streamlit.components.v1 as components
 import plotly.graph_objects as go
-
+import pygame # Usado para pegar largura da tela, tefinir tamanho do gráfico
 
 # Salva modelo treinado em arquivo no disco
 from joblib import dump, load
@@ -57,6 +57,15 @@ data_chart_groups.columns = ["clusters","Extroversão","Neuroticismo","Amabilida
 # st.write(fig)
 
 # Gráfico de linhas com plotly
+# pegando tamanho da tela
+
+pygame.init()
+info = pygame.display.Info()
+screen_width = info.current_w
+screen_height = info.current_h
+
+
+# Gráfico
 fig = go.Figure()
 for i in range(5):
     fig.add_trace(go.Scatter(x=data_chart_groups.columns[1:], y=list(data_chart_groups.iloc[i, 1:]), mode='lines', name=f'Grupo {i+1}'))
@@ -65,6 +74,7 @@ fig.update_layout(
     font=dict(size=15),
     autosize=False,
     height=260,
+    width=screen_width/2,
     xaxis_title="", 
     margin=dict(l=0, r=0, t=30, b=20)
 )
@@ -77,13 +87,13 @@ fig.update_yaxes(
 st.write(fig)
 
 
-components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
-        Todos esses traços de personalidade estão presentes em cada pessoa. 
-        O gráfico de linhas acima mostra cinco grupos de pessoas, de acordo 
-        com cada nível dos traços de personalidade. Responda às perguntas na barra lateral 
-        para ver a qual grupo corresponde ao seu perfil.
-    </p>""", height=100)
+# components.html("""
+#     <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
+st.write("""Todos esses traços de personalidade estão presentes em cada pessoa. 
+O gráfico de linhas acima mostra cinco grupos de pessoas, de acordo 
+com cada nível dos traços de personalidade. Responda às perguntas na barra lateral 
+para ver a qual grupo corresponde ao seu perfil.""")
+    # </p>""", height=100)
 st.markdown("""---""")
 
 
@@ -105,7 +115,7 @@ def run_algorithm():
     profile_group = model.predict(user_inputs)[0] # kmeans
     
     group_ident = f'Seu perfil corresponde ao Grupo {profile_group + 1}'
-    st.markdown(f"<h2 style='text-align: center'>{group_ident}</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h1 style='text-align: center'>{group_ident}</h1>", unsafe_allow_html=True)
 
 
     # Gráfico de área do perfil corespondente
@@ -116,6 +126,7 @@ def run_algorithm():
         xaxis_title="", yaxis_title="",
         autosize=False,
         height=300,
+        width=screen_width/2,
     )
     fig.update_yaxes(
         tickvals=[2, 2.5, 3, 3.5, 4],
@@ -137,28 +148,25 @@ col1, col2 = st.beta_columns([1,3])
 
 col1.image("img/extroversion.png")
 with col2:
-    components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
+    st.write("""
         (Extroversão) - Pessoas com alto índice de extroversão são extrovertidas e tendem a ganhar energia em situações 
         sociais. Estar perto de outras pessoas os ajuda a se sentirem energizados e animados. Pessoas com baixa extroversão 
         (ou introvertidas) tendem a ser mais reservadas e têm menos energia para gastar em ambientes sociais. Os eventos 
         sociais podem parecer exaustivos e os introvertidos costumam exigir um período de solidão e sossego para 
         'recarregar as energias'.
-    </p>""", height=225)
-
+        """)
+    
 st.markdown("""---""")
 col1, col2 = st.beta_columns([1,3])
 
 
 col1.image("img/neuroticism.png")
 with col2:
-    components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
-        (Neuroticismo) - Neuroticismo é um traço caracterizado por tristeza, 
+    st.write("""(Neuroticismo) - Neuroticismo é um traço caracterizado por tristeza, 
         mau humor e instabilidade emocional. Indivíduos com alto teor dessa característica tendem a sofrer oscilações 
         de humor, ansiedade, irritabilidade e tristeza. Aqueles com baixo nível dessa característica tendem a ser mais 
         estáveis ​​e emocionalmente resilientes.
-    </p>""")
+        """)
 
 st.markdown("""---""")
 col1, col2 = st.beta_columns([1,3])
@@ -166,34 +174,31 @@ col1, col2 = st.beta_columns([1,3])
 
 col1.image("img/agreeableness.png")
 with col2:
-    components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
+    st.write("""
         (Amabilidade) - Essa dimensão da personalidade inclui atributos como confiança, altruísmo, gentileza, afeto e outros comportamentos pró-sociais. Pessoas com alto nível de gentileza tendem a ser mais cooperativas, enquanto aqueles com baixo nível desse traço tendem a ser mais competitivos e às vezes até manipuladores.
-    </p>""")
+        """)
 
 st.markdown("""---""")
 col1, col2 = st.beta_columns([1,3])
 
 col1.image("img/conscientiousness.png")
 with col2:
-    components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
+    st.write("""
         (Consciência) - As características padrão dessa dimensão incluem altos níveis de consideração, bom controle de 
         impulsos e comportamentos direcionados a objetivos. Pessoas altamente conscienciosas tendem a ser organizadas 
         e atentas aos detalhes. Eles planejam com antecedência, pensam sobre como seu comportamento afeta os outros e 
         estão atentos aos prazos.
-    </p>""", height=180)
+        """)
 
 st.markdown("""---""")
 col1, col2 = st.beta_columns([1,3])
 
 col1.image("img/openness.png")
 with col2:
-    components.html("""
-    <p style="text-align: justify; font-family:sans-serif; font-size: 1rem; font-weight: 400; color: rgb(38, 39, 48); line-height: 1.6">
+    st.write("""
         (Abertura) - Esse traço apresenta características como imaginação e percepção.
         Pessoas com alto valor nesse traço também tendem a ter uma ampla gama de interesses. Eles são curiosos sobre o mundo e outras pessoas e estão ansiosos para aprender coisas novas e desfrutar de novas experiências.
         Pessoas com alto nível desse atributo tendem a ser mais aventureiras e criativas. Pessoas com baixo nível dessa característica costumam ser muito mais tradicionais e podem ter dificuldades com o pensamento abstrato.
-    </p>""", height=235)
+        """)
 
 st.write("fonte: https://www.verywellmind.com/")
